44232bf9287079661ed2bf8c625f7591233f71a9
Guy-Sylvestre <guysylvestre876@gmail.com>
2020-12-29 13:22:06 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+function full() {
+    document.getElementById("colomn1").style.width = "100%";
+    document.getElementById("colomn2").style.width = "100%";
+    document.getElementById("colomn3").style.width = "100%";
+    document.getElementById("colomn4").style.width = "100%";
+}
+
+function twocolomns() {
+    document.getElementById("colomn1").style.width = "50%";
+    document.getElementById("colomn2").style.width = "50%";
+    document.getElementById("colomn3").style.width = "50%";
+    document.getElementById("colomn4").style.width = "50%";
+}
+
+function forcolomns() {
+    document.getElementById("colomn1").style.width = "25%";
+    document.getElementById("colomn2").style.width = "25%";
+    document.getElementById("colomn3").style.width = "25%";
+    document.getElementById("colomn4").style.width = "25%";
+}
